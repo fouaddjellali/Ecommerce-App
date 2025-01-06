a132a6cb2b399abc65dcf22dd7caaef9ec8cb1b8
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2025-01-06 21:51:06 +0100

    maj global version + page accueil
    + dockerisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        // Données dynamiques pour les catégories et les produits
+        $categories = ['Catégorie 1', 'Catégorie 2', 'Catégorie 3'];
+        $featuredProducts = [
+            ['name' => 'Produit 1', 'description' => 'Description du produit 1'],
+            ['name' => 'Produit 2', 'description' => 'Description du produit 2'],
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+            'featuredProducts' => $featuredProducts,
+        ]);
     }
 }
