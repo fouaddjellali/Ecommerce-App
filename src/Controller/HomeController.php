f6509fe7ed1f4b4f2b2aa6e07edfc86a84cf10e7
Tinou95 <quentin95170@hotmail.fr>
2025-02-02 00:26:40 +0100

    feat: crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        // Données dynamiques pour les catégories et les produits
-        $categories = ['Catégorie 1', 'Catégorie 2', 'Catégorie 3'];
-        $featuredProducts = [
-            ['name' => 'Produit 1', 'description' => 'Description du produit 1'],
-            ['name' => 'Produit 2', 'description' => 'Description du produit 2'],
-        ];
+        // Fetch categories from the database
+        $categories = $entityManager
+            ->getRepository(Category::class)
+            ->findAll();
+
+        // Fetch featured products from the database (you may want to adjust this query if you have specific criteria for featured products)
+        $featuredProducts = $entityManager
+            ->getRepository(Product::class)
+            ->findBy([], ['id' => 'DESC'], 5);  // Get the last 5 products (or modify this to suit your needs)
 
         return $this->render('home/index.html.twig', [
             'categories' => $categories,
